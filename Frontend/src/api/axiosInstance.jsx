32e887d287b4a661b523f02d1e763c996c8eb449
shijiaa <shijihcl@gmail.com>
2024-08-05 17:27:16 +0530

    Final changes done

--- Frontend/src/api/axiosInstance.jsx
+++ Frontend/src/api/axiosInstance.jsx
@@ -1,8 +1,8 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-  //baseURL: "http://localhost:3000/api/users",
-  baseURL: "https://castme-project-1.onrender.com/api/users",
+  baseURL: "http://localhost:3000/api/users",
+ // baseURL: "https://castme-project-1.onrender.com/api/users",
 });
 
 // Request interceptor
